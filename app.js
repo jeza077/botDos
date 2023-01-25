1f7ae434b1ed9b951833960bef961b3d4e73f1bf
JEZA <jesus.zuniga077@gmail.com>
2023-01-24 23:52:03 -0600

    cambios

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const { createProvider } = require('packages/bot/index')
+const { createProvider } = require('./packages/bot/index');
 
-const MetaProvider = require('packages/provider/src/meta/index')
-const MockAdapter = require('packages/database/src/mock/index')
+const MetaProvider = require('./packages/provider/src/meta/index')
+const MockAdapter = require('./packages/database/src/mock/index')
 
-const { createBotDialog } = require('packages/contexts/src/dialogflow/index')
+const { createBotDialog } = require('./packages/contexts/src/dialogflow/index')
+
 
 /**
  * Aqui declaramos los flujos hijos, los flujos se declaran de atras para adelante, es decir que si tienes un flujo de este tipo:
